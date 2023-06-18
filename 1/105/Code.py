70ac04bdd2467ff6558b03d6d532ec3561cd7c1d
gramado <fabiano.oliveira@ime.uerj.br>
2023-06-18 12:40:03 -0300

    ex 105

---
+++ 1/105/Code.py
@@ -0,0 +1,44 @@
+.:75Hj1*YH1587:<H-6<:),)
+
+,-.H;75)'41;<)P@THAQb
+	KH+7,->
+	>)1'=5HeHX
+	:-;8HeH#%
+	tHeH5)@P4-6P@QTH4-6PAQQ
+	.7:H1H16H:)6/-PXTHtSYQb
+		1.H1HdH4-6P@QbH
+			,@HeH@#1%
+		-4;-b
+			,@HeHX
+		1.H1HdH4-6PAQbH
+			,AHeHA#1%
+		-4;-b
+			,AHeHX
+		:-;8V)88-6,PP,@S,ASH>)1'=5QMYXQ
+		>)1'=5HeHP,@S,AS>)1'=5QWWYX
+
+	KH:-57>-HB-:7;HàH-;9=-:,)H,)H:-8:-;-6<)çã7H,-+15)4H,7H:-;=4<),7
+	1.H4-6P:-;8QHfHYH)6,H:-;8#t%HeeHXb
+		:-;8V878PQ
+	:-<=:6H:-;8
+	KH+7,->
+
+?014-HP|:=-Qb
+	<:Ab
+		@HeH;<:P-6<:),)PQQ
+		1.H@HIeHOh-6<:),)/:)6,-Ob
+			AHeH;<:P-6<:),)PQQ
+		-4;-b
+			@HeHOaORYXXXXXX
+			AHeHOaORYXXXXXX
+		@'41;<)HeH#%
+		A'41;<)HeH#%
+		.7:H1H16H@b
+			@'41;<)V)88-6,P16<P1QQ
+		.7:H1H16HAb
+			A'41;<)V)88-6,P16<P1QQ
+	-@+-8<b
+		*:-)3
+	
+	:HeHOOV2716P#;<:P+QH.7:H+H16H;75)'41;<)P@'41;<)THA'41;<)Q%Q
+	8:16<P:Q
