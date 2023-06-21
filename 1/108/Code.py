71a7c6a1170d86374475da412273ee1f241f9d40
gramado <fabiano.oliveira@ime.uerj.br>
2023-06-21 11:54:20 -0300

    ex 108

--- 1/108/Code.py
+++ 1/108/Code.py
@@ -16,36 +16,42 @@ KH+7,->:-57>-
 			,AHeHX
 		:-;8V)88-6,PP,@S,ASH>)1'=5QMYXQ
 		>)1'=5HeHP,@S,AS>)1'=5QWWYX
+
+	KH:-57>-HB-:7;HàH-;9=-:,)H,)H:-8:-;-6<)çã7H,-+15)4H,7H:-;=4<),7
+	1.H4-6P:-;8QHfHYH)6,H:-;8#t%HeeHXb
+		:-;8V878PQ
+	:-<=:6H:-;8
+	
+,-.H5=4<1841+)'41;<)P@THAQb
+	>)1HeHX
+	:-;8HeH#%
+	tHeH4-6P@Q
+	.7:H1H16H:)6/-PXTHtSYQb
+		1.H1HdHtbH
+			,@HeH@#1%
+		-4;-b
+			,@HeHX
+		:-;8V)88-6,PP,@RAHSH>)1QMYXQ
+		>)1HeHPP,@RAQS>)1QWWYX
+
+	KH:-57>-HB-:7;HàH-;9=-:,)H,)H:-8:-;-6<)çã7H,-+15)4
+	1.H4-6P:-;8QHfHYH)6,H:-;8#t%HeeHXb
+		:-;8V878PQ
+
 	:-<=:6H:-;8
 KH+7,->:-57>-
 
-,-.H5=4<1841+)'41;<)P@THAQb
+,-.H5=4<1841+)'41;<)'41;<)P@THAQb
 	KH+7,->
 	>)1HeHX
 	:-;8HeH#X%HRHP4-6P@QHSH4-6PAQQ
 
-	.7:H1H16H:)6/-PXTH4-6P@QSYQb
-		<-58HeH#%
-		1.H1HdH4-6P@QbH
-			,@HeH@#1%
-		-4;-b
-			,@HeHX
-		.7:H2H16H:)6/-PXTH4-6PAQSYQb
-			1.H2HdH4-6PAQbH
-				,AHeHA#2%
-			-4;-b
-				,AHeHX
-			<-58V)88-6,PP,@R,ASH>)1QMYXQ
-			>)1HeHPP,@R,AQS>)1QWWYX
-			
-		KH),1+176)HB-:7H67H.16)4H8):)H;75):
-		1.H1HIeHXb
-			.7:H3H16H:)6/-PXTH1Qb
-				<-58V16;-:<PXTHXQ
-		:-;8HeH;75)'41;<)P:-;8TH<-58Q
+	.7:H1H16H:)6/-P4-6PAQQb
+		8):+-4)HeH#X%R1HSH5=4<1841+)'41;<)P@THA#1%Q
+		:-;8HeH;75)'41;<)P:-;8TH8):+-4)Q		
 
-	KH:-57>-HB-:7;HàH-;9=-:,)
-	?014-H4-6P:-;8QHfHYH)6,H:-;8#UY%HeeHXb
+	KH:-57>-HB-:7;HàH-;9=-:,)H,)H:-8:-;-6<)çã7H,-+15)4H,7H:-;=4<),7
+	?014-H4-6P:-;8QHfHYH)6,H:-;8#4-6P:-;8QUY%HeeHXb
 		:-;8V878PQ
 					
 	:-<=:6H:-;8
@@ -54,7 +60,11 @@ KH+7,->:-57>-
 ?014-HP|:=-Qb
 	<:Ab
 		@HeH;<:P-6<:),)PQQ
-		AHeH;<:P-6<:),)PQQ
+		1.H@HIeHJh-6<:),)/:)6,-Jb
+			AHeH;<:P-6<:),)PQQ
+		-4;-b
+			@HeHJaJRYXXX
+			AHeHJaJRYXXX
 		@'41;<)HeH#%
 		A'41;<)HeH#%
 		.7:H1H16H:->-:;-,P@Qb
@@ -63,4 +73,7 @@ KH+7,->:-57>-
 			A'41;<)V)88-6,P16<P1QQ
 	-@+-8<b
 		*:-)3
-	8:16<P5=4<1841+)'41;<)P@'41;<)THA'41;<)QQ
+	:HeH5=4<1841+)'41;<)'41;<)P@'41;<)THA'41;<)QcH:V:->-:;-PQc
+	:HeHOOV2716P#;<:P+QH.7:H+H16H:%Q
+	8:16<P:Q	
+
