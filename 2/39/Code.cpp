dd1d27072d3d6f6187be2784222368f47aaf3a31
gramado <fabiano.oliveira@ime.uerj.br>
2023-08-18 11:27:23 -0300

    ex 39

---
+++ 2/39/Code.cpp
@@ -0,0 +1,38 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+
+16<RHxoP16<H)TH16<H9TH16<H6QC
+	WWH+7,->
+	16<RH:-;8c
+	16<H1c
+	
+	:-;8HeHP16<RQ+)447+P6TH;1B-7.P16<QQc
+	
+	:-;8#X%HeH)c
+	
+	.7:HP1HeHYcH1HdH6cH1SSQHC
+		:-;8#1%HeH:-;8#1UY%HRH9c
+	E
+	
+	:-<=:6H:-;8c
+	WWH+7,->
+E
+
+16<H5)16P>71,QC
+	16<H1TH)TH9TH6THR:-;8c
+
+	?014-HP;+)6.PJM,HM,HM,JTHN)THN9THN6QHfHXQHC
+		:-;8HeHxoP)TH9TH6Qc
+	
+		8:16<.PJ#JQc
+		.7:HP1HeHXcH1HdH6cH1SSQHC
+			1.HP1fXQH8:16<.PJTHJQc
+			8:16<.PJM,JTH:-;8#1%Qc
+		E
+		8:16<.PJ%$6JQc
+		.:--P:-;8Qc
+	E
+	
+	:-<=:6HXc
+E
+
