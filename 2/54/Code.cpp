c10de63fe4311ac61588eff4db67ad60f5ea2c9c
gramado <fabiano.oliveira@ime.uerj.br>
2023-08-18 14:57:33 -0300

    ex 54

---
+++ 2/54/Code.cpp
@@ -0,0 +1,43 @@
+K16+4=,-Hd;<,17V0f
+
+WWH+7,->:-57>-
+,7=*4-H,1;<8-:+7::1,)P,7=*4-H>XTH,7=*4-H)TH,7=*4-H<QC
+	:-<=:6HP>XR<QHSHP)R<R<QWZc
+E
+WWH+7,->:-57>-
+
+,7=*4-H+)4+',1;<P,7=*4-H>TH,7=*4-H)TH,7=*4-H<QC
+	WWH+7,->
+	,7=*4-H:-;8c
+	
+	1.HP)feXQHC
+		:-;8HeH,1;<8-:+7::1,)P>T)T<Qc
+	EH-4;-HC
+		,7=*4-H<8c
+		<8HeHU>W)c
+		1.HP<8HfeH<QHC
+			:-;8HeH,1;<8-:+7::1,)P>T)T<Qc
+		EH-4;-HC
+			,7=*4-H,YTH,Zc
+			
+			,YHeHUP>R>QWPZR)Qc
+			,ZHeH,1;<8-:+7::1,)PXTU)T<U<8Qc
+			
+			:-;8HeH,YS,Zc
+		E
+	E
+	
+	:-<=:6H:-;8c
+	WWH+7,->
+E
+
+16<H5)16PQC
+	,7=*4-H>TH)TH<c
+	
+	?014-HP;+)6.PJM4.HM4.HM4.JTHN>THN)THN<QfXQHC
+		8:16<.PJMVY^/$6JTH+)4+',1;<P>TH)TH<QQc
+	E
+	
+	:-<=:6HXc
+E
+
