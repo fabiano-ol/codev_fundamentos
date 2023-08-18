30ef6ab09113911c1a841be8ee0ba98148a5446b
gramado <fabiano.oliveira@ime.uerj.br>
2023-08-18 19:24:24 -0300

    ex 6

--- 2/6/Code.cpp
+++ 2/6/Code.cpp
@@ -1,6 +1,5 @@
 K16+4=,-Hd;<,17V0f
 K16+4=,-Hd;<,41*V0f
-K16+4=,-Hd5)<0V0f
 K16+4=,-Hd;<:16/V0f
 
 >71,Hz->-:<-:P+0):H@#%TH16<H6QHC
@@ -11,23 +10,24 @@ K16+4=,-Hd;<:16/V0f
 	E
 E
 
-+0):HRH;75)'41;<)P+0):H@#%TH16<H6@TH+0):HA#%TH16<H6ATH16<HNH6QHC
++0):HRH;=*<:)1'41;<)P+0):H@#%TH16<H6@TH+0):HA#%TH16<H6ATH16<HNH6QC
 	WWH+7,->
-	16<H>)1'=5TH1c
-	+0):HRH:-;8c
-
+	16<H-58:-;<),7TH1TH;=*c
+	+0):HR:-;8c
+	
+	-58:-;<),7HeHXc
+	
 	1.HP6@HfH6AQHC
-		6HeH6@SYc
+		6HeH6@c
 	EH-4;-HC
-		6HeH6ASYc
+		6HeH6Ac
 	E
 	
-	:-;8HeHP+0):RQH+)447+P6SYTH;1B-7.P+0):QQc
-
-	>)1'=5HeHXc
+	:-;8HeHP+0):RQ+)447+P6SYTH;1B-7.P+0):QQc
+	
 	.7:HP1HeHXcH1HdH6cH1SSQHC
-		16<H,@TH,Ac
-		
+		16<H,@TH,Ac	
+					
 		1.HP1HdH6@QHCH
 			,@HeH@#1%UOXOc
 		EH-4;-HC
@@ -39,19 +39,25 @@ E
 		EH-4;-HC
 			,AHeHXc
 		E
-
-		:-;8#1%HeHOXOSPP,@S,AS>)1'=5QMYXQc
 		
-		>)1'=5HeHP,@S,AS>)1'=5QWYXcH
+		;=*HeH,@U,AU-58:-;<),7c
+
+		1.HP;=*HdHXQHC
+			;=*HeH;=*HSHYXc
+			-58:-;<),7HeHYc
+		EH-4;-HC
+			-58:-;<),7HeHXc
+		E
+		
+		:-;8#1%HeHOXOHSH;=*c
 	E
-	1.HP:-;8#6UY%HeeHOXOQC
+	?014-HPP6HfHYQHNNHP:-;8#6UY%HeeHOXOQQHC
 		6HeH6UYc
 	E
 	:-;8#6%HeHO$XOc
-	
 	:-<=:6H:-;8c
 	WWH+7,->
-E
+EH
 
 16<H5)16PQC
 	+0):HRH@THRHATHRH:-;8c
@@ -60,20 +66,25 @@ E
 	AHeHP+0):HRQH+)447+PYXXXXXYTH;1B-7.P+0):QQc
 	
 	?014-HP;+)6.PJM;JTH@QHfHXQHC
-		1.HP;<:+58P@THJh-6<:),)/:)6,-JQHIeHXQHC
-			;+)6.PJM;JTHAQc
-		EH-4;-HC
+		1.HP;<:+58P@THJh-6<:),)/:)6,-YJQHeeHXQHC
+			.7:HP16<H1eXcH1dYXXXXXXUYcH1SSQHC
+				@#1%HeHOYOcHA#1%HeHOaOc
+			E
+			@#YXXXXXXUY%HeHOYOcH@#YXXXXXX%HeHO$XOcHA#YXXXXXXUY%HeHO$XOcH
+		EH-4;-H1.HP;<:+58P@THJh-6<:),)/:)6,-ZJQHeeHXQHC
 			.7:HP16<H1eXcH1dYXXXXXXcH1SSQHC
-				@#1%HeHOaOcHA#1%HeHOaOc
+				@#1%HeHOYOcHA#1%HeHOYOc
 			E
 			@#YXXXXXX%HeHO$XOcHA#YXXXXXX%HeHO$XOcH
+		EH-4;-HC
+			;+)6.PJM;JTHAQc
 		E
 		16<H6@TH6ATH6TH1TH2c
 		6@HeH;<:4-6P@QcH6AHeH;<:4-6PAQc
 		
 		z->-:<-:P@TH6@QcHz->-:<-:PATH6AQcH
 
-		:-;8HeH;75)'41;<)P@TH6@THATH6ATH6Qc
+		:-;8HeH;=*<:)1'41;<)P@TH6@THATH6ATH6Qc
 		
 		z->-:<-:P:-;8TH6Qc
 		
@@ -85,5 +96,6 @@ E
 	.:--PAQc
 	
 	:-<=:6HXc
+
 E
 
