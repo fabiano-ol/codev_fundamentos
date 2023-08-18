46b392a0a8acd661c1ef994cde8c7de2bff227b5
gramado <fabiano.oliveira@ime.uerj.br>
2023-08-18 15:18:21 -0300

    ex 55

---
+++ 2/55/Code.cpp
@@ -0,0 +1,56 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+K16+4=,-Hd;<:16/V0f
+
+*774H5)17:1/=)4'>-<7:P+0):H@#%TH16<H6@TH+0):HA#%TH16<H6AQHC
+	WWH+7,->
+	16<H1TH5)@cH+0):H,@TH,Ac
+	
+	1.HP6@HfH6AQHC
+		5)@HeH6@c
+	EH-4;-HC
+		5)@HeH6Ac
+	E
+	
+	.7:HP1HeH5)@UYcH1HfeHXcH1UUQHC
+		1.HP1HdH6@QHC
+			,@HeH@#1%c
+		E-4;-C
+			,@HeHOXOc
+		E
+		
+		1.HP1HdH6AQHC
+			,AHeHA#1%c
+		EH-4;-HC
+			,AHeHOXOc
+		E
+		
+		1.HPP,@HfH,AQHDDHP,@HeeH,AHNNH1HeeHXQQHC
+			:-<=:6H<:=-c
+		EH-4;-H1.HP,@HdH,AQHC
+			:-<=:6H.)4;-cH
+		E
+	E
+	:-<=:6H.)4;-c
+	WWH+7,->
+E
+
++0):H@#YXXX%THA#YXXX%c
+
+16<H5)16PQHC
+	
+	?014-HP;+)6.PJHM#&$6%JTH@QfXQHC
+		1.HP;<:+58P@THJh-6<:),)/:)6,-JQIeXQHC
+			;+)6.PJHM#&$6%JTHAQc
+		EH-4;-HC
+			.7:HP16<H1eXcH1daaacH1SSQHC
+				@#1%eYcHA#1%eYc
+			E
+			A#X%eZcHA#aaa%eO$XOcH@#aaa%eO$XOcH
+		E
+				
+		8:16<.PJM,$6JTH5)17:1/=)4'>-<7:P@TH;<:4-6P@QTHATH;<:4-6PAQQQc
+	E
+	
+	:-<=:6HXc
+E
