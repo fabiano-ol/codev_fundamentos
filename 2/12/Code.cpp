b0c9469e953dd15bfcae5992519ec45ce3585b67
gramado <fabiano.oliveira@ime.uerj.br>
2023-08-18 21:11:18 -0300

    ex 12

---
+++ 2/12/Code.cpp
@@ -0,0 +1,61 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+K16+4=,-Hd5)<0V0f
+K16+4=,-Hd;<:16/V0f
+
+,7=*4-H+76>-:<-P16<H@TH+0):H=61,),-Y#%TH+0):H=61,),-Z#%QHC
+	WWH+7,->
+	,7=*4-H*=;+)c
+	16<H1TH78TH16,1+-YTH16,1+-Zc
+	
+	+0):H=61,),-;#[%#_%#\%HeHCCJ55JTHJ+5JTHJ,5JTHJ5JTHJ,)5JTHJ05JTHJ35JET
+			CJ5/JTHJ+/JTHJ,/JTHJ/JTHJ,)/JTHJ0/JTHJ3/JET
+			CJ54JTHJ+4JTHJ,4JTHJ4JTHJ,)4JTHJ04JTHJ34JEEc
+	
+	*=;+)HeHXc
+	
+	.7:HP1HeHXcH1HdH[cH1SSQHC
+		16<H2c
+		
+		.7:HP2HeHXcH2HdH_cH2SSQHC
+			1.HP;<:+58P=61,),-;#1%#2%TH=61,),-YQHeeHXQHC
+				78HeH1c
+				*=;+)HeHYc
+				16,1+-YHeH2c
+				*:-)3cH
+			E
+		E
+	E
+	
+	1.HP*=;+)HeeHYQHC
+		16<H2c
+		*=;+)HeHXc
+		.7:HP2HeHXcH2HdH_cH2SSQHC
+			16<H3c
+			1.HP;<:+58P=61,),-;#78%#2%TH=61,),-ZQHeeHXQHC
+				*=;+)HeHYc
+				16,1+-ZHeH2c
+				*:-)3c
+			E
+		E
+	E
+	
+	1.HP*=;+)HeeHXQHC
+		:-<=:6HXc
+	EH-4;-HC
+		:-<=:6H16<P@HRH87?PYXTH16,1+-YU16,1+-ZQQc
+	E
+	WWH+7,->
+E
+
+16<H5)16PQHC
+	16<H@c
+	+0):H=61,),-Y#\%TH=61,),-Z#\%c
+
+	?014-HP;+)6.PJM,JTHN@QfXQHC	
+		;+)6.PJM;JTH=61,),-YQcH;+)6.PJM;JTH=61,),-ZQc
+		8:16<.PJMVY^/$6JTH+76>-:<-P@TH=61,),-YTH=61,),-ZQQc
+	E
+		
+	:-<=:6HXc
+E
